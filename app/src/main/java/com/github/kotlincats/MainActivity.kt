2bc543175136974c4de8d15032a9adde3ef5bb1c
kashifo <kashifanwaar@casperon.in>
2019-09-18 00:26:09 +0530

    Added volley singleton, stringRequest GET API to load images, show images via Glide, button click to load another image.

--- app/src/main/java/com/github/kotlincats/MainActivity.kt
+++ app/src/main/java/com/github/kotlincats/MainActivity.kt
@@ -1,12 +1,65 @@
 package com.github.kotlincats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = javaClass.simpleName
+    val catUrl = "https://aws.random.cat/meow"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e(TAG, "onCreate")
+
+        showRandomCat()
+
+        btnNextCat.setOnClickListener {
+            showRandomCat()
+        }
     }
+
+    fun showRandomCat() {
+
+        val requestCat = StringRequest(Request.Method.GET, catUrl, Response.Listener { response ->
+
+            try {
+                val jsonObject = JSONObject(response)
+                val file = jsonObject.getString("file")
+
+                if (!file.isNullOrEmpty()) {
+                    Glide.with(this).load(file).placeholder(R.drawable.loading).dontAnimate()
+                        .error(R.drawable.ic_warning).into(imgCat)
+                    tvUrl.text = file
+                } else {
+                    showEmpty()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                showEmpty()
+            }
+
+        }, Response.ErrorListener { error ->
+            Log.e(TAG, "onErrorResponse=${error?.message}")
+            showEmpty()
+        })
+
+        imgCat.setImageResource(R.drawable.loading) //show loading
+        MyAppClass.instance?.addToRequestQueue(requestCat)
+
+    }
+
+    fun showEmpty() {
+        tvUrl.text = "Something Error"
+        Glide.with(this).load(R.drawable.ic_warning).into(imgCat)
+    }
+
 }
---
+++ app/src/main/java/com/github/kotlincats/MyAppClass.kt
@@ -0,0 +1,35 @@
+package com.github.kotlincats
+
+import android.app.Application
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class MyAppClass : Application() {
+
+    companion object {
+        private val TAG = MyAppClass::class.java.simpleName
+        @get:Synchronized
+        var instance: MyAppClass? = null
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    val requestQueue: RequestQueue? = null
+        get() {
+            if (field == null) {
+                return Volley.newRequestQueue(applicationContext)
+            }
+            return field
+        }
+
+    fun <T> addToRequestQueue(request: Request<T>) {
+        request.tag = TAG
+        requestQueue?.add(request)
+    }
+
+}
